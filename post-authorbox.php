c4e92709989a8a8422a834be8747f4bf7f81fe72
Ryan Poe <baublet@gmail.com>
2018-10-22 12:58:31 -0500

    Updates

--- post-authorbox.php
+++ post-authorbox.php
@@ -2,11 +2,11 @@
     <a href="<?=get_author_posts_url($author_id)?>" class="float-left mr-small mb-small">
          <?=get_avatar(get_the_author_meta('user_email', $author_id))?>
      </a>
-	<a rel="author" href="<?=get_author_posts_url($author_id)?>" class="author-name text-normal text-bold">
+	<a rel="author" href="<?=get_author_posts_url($author_id)?>" class="author-name text-base font-bold">
 		<?=get_the_author_meta('display_name', $author_id)?>
 	</a>
 	<div class="description">
         <?=get_the_author_meta('affiliation', $author_id); ?>
     </div>
-	<a href="<?=get_author_posts_url($author_id)?>" class="block mt-small">View all posts by <?=get_the_author_meta('display_name', $author_id)?> &raquo;</a>
+	<a href="<?=get_author_posts_url($author_id)?>" class="block mt-small font-bold text-xs">View all posts by <?=get_the_author_meta('display_name', $author_id)?> &raquo;</a>
 </div>
