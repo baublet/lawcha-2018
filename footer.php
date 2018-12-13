c79a322d9b0a5a2f5d0d5be7c8defaa8fbf92860
Ryan Poe <baublet@gmail.com>
2018-12-12 21:40:19 -0600

    Theme updates

--- footer.php
+++ footer.php
@@ -1,32 +1,32 @@
 </div><!-- .content -->
 
-<footer id="colophon" class="footer text-background bg-foreground py-large">
+<footer id="colophon" class="footer text-background bg-foreground py-large text-sm">
     <div class="contentBlock px-normal">
         <div class="social-media desktop:flex">
             <div class="twitter desktop:w-1/3">
-                <h2><a href="https://twitter.com/lawcha_org"><span class="icon-twitter"></span> Twitter</a></h2>
-                <p><span>Follow us on Twitter</span> <a href="https://twitter.com/lawcha_org">@LAWCHA_ORG</a> or <a href="https://twitter.com/laboronline">@LaborOnline</a> to take part in the discussion.</p>
+                <h2 class="font-heading text-lg font-bold mb-small"><a href="https://twitter.com/lawcha_org" class="footerLink"><span class="icon-twitter"></span> Twitter</a></h2>
+                <p><span>Follow us on Twitter</span> <a href="https://twitter.com/lawcha_org" class="footerLink">@LAWCHA_ORG</a></p>
             </div>
-            <div class="facebook desktop:w-1/3">
-                <h2><a href="https://www.facebook.com/laborandworkingclasshistory"><span class="icon-facebook"></span> Facebook</a></h2>
-                <p><span>Follow us on</span> <a href="https://www.facebook.com/laborandworkingclasshistory">Facebook page</a> to receive the latest updates. Don't forget to like us!</p>
+            <div class="facebook desktop:w-1/3 mt-large desktop:mt-0">
+                <h2 class="font-heading text-lg font-bold mb-small"><a href="https://www.facebook.com/laborandworkingclasshistory" class="footerLink"><span class="icon-facebook"></span> Facebook</a></h2>
+                <p><span>Follow us on</span> <a href="https://www.facebook.com/laborandworkingclasshistory" class="footerLink">Facebook page</a> to receive the latest updates. Don't forget to like us!</p>
             </div>
-            <div class="news-categories desktop:w-1/3">
-                <h2><span class="icon-folder"></span> News Categories</h2>
-                <?=wp_nav_menu(array("menu" => "News Categories"))?>
+            <div class="footer-news-categories desktop:w-1/3 mt-large desktop:mt-0">
+                <h2 class="font-heading text-lg font-bold mb-small"><span class="icon-folder"></span> News Categories</h2>
+                <?= wp_nav_menu(["menu" => "News Categories"]) ?>
             </div>
         </div>
-        <div class="site-info mt-large">
+        <div class="site-info mt-large desktop:text-center text-sx">
             <p>&copy 1998-Present LAWCHA. All Rights Reserved.</p>
 
             <p>Supported for the <strong>latest versions</strong> of:
-                <a href="http://www.google.com/chrome/" class="icon-chrome"><span>Chrome</span></a>
-                <a href="https://www.mozilla.org/en-US/firefox/new/" class="icon-firefox"><span>Firefox<span></span></a>
-                <a href="https://www.microsoft.com/en-us/windows/microsoft-edge" class="icon-IE"><span>Edge</span></a>
-                <a href="http://www.opera.com/" class="icon-opera"><span>Opera</span></a>
+                <a href="http://www.google.com/chrome/" class="footerLink"><span>Chrome</span></a>
+                <a href="https://www.mozilla.org/en-US/firefox/new/" class="footerLink"><span>Firefox<span></span></a>
+                <a href="https://www.microsoft.com/en-us/windows/microsoft-edge" class="footerLink"><span>Edge</span></a>
+                <a href="http://www.opera.com/" class="footerLink"><span>Opera</span></a>
             </p>
 
-            <p><strong><a href="#top">Return to Top</a></strong></p>
+            <p class="mt-large"><strong><a href="#top" class="footerLink">Return to Top</a></strong></p>
         </div>
     </div>
 </footer>
