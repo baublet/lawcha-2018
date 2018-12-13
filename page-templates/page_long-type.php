3415199e3421b8f2823742be05cb0c3a07643038
Ryan Poe <baublet@gmail.com>
2018-12-12 22:43:19 -0600

    Theme updates

--- page-templates/page_long-type.php
+++ page-templates/page_long-type.php
@@ -4,7 +4,7 @@
 get_header(); ?>
 
 	<div id="primary" class="content-area">
-		<main id="main" class="site-main type longForm" role="main">
+		<main id="main" class="site-main type" role="main">
 
 		<?php
 		// Start the loop.
